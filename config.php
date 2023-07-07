a49fa1ba51ef2155dcfd654a45c41b4fd8f343fc
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2023-07-07 21:58:12 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$token = "00d2a56d5d3693dbe986a07e15b0fd777576fade";
+$PHPSESSID = "PHPSESSID=16lqqfs707551lrsd03hv3n0ej";
+$cfduid = "_gid=GA1.2.187766069.1688737199";
+$useragent = "Mozilla/5.0 (Linux; Android 8.1.0; SM-J260G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
